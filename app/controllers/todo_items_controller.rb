edf313f81a1039dccdf46a051cb8373af2aab600
Dawna Ara <dawnaara@gmail.com>
2016-08-24 11:25:53 -0700

    styling complete

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -4,9 +4,7 @@ class TodoItemsController < ApplicationController
 
 	def create
 		@todo_item = @todo_list.todo_items.create(todo_item_params)
-
-		redirect_to @todo_list
-			
+		redirect_to @todo_list		
 	end
 
 	def complete
